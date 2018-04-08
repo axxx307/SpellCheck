44130f7635e55aadfb03b0b3b7a0dbd2ba5665fe
axxx307 <xuj1337@gmail.com>
2018-04-08 20:48:36 +0700

    remake implementation to use HashSet and Hash in general

--- SpelingClasses/SymSpell.cs
+++ SpelingClasses/SymSpell.cs
@@ -5,25 +5,21 @@ using System.Linq;
 using System.Net;
 using System.Security.Cryptography.X509Certificates;
 using Castle.DynamicProxy.Generators.Emitters.SimpleAST;
-using ShellProgressBar;
 
 namespace spell_check.SpelingClasses
 {
     public class SymSpell
     {
         private int _editDistance;
-        private Dictionary<string, List<string>> _generatedEdtis;
+        private int Threshold = 300000;
+        private Dictionary<int, string[]> _generatedEdtis;
         private Dictionary<string, long> _generatedFrequencies;
         public SymSpell(int editDistance)
         {
             _editDistance = editDistance;
-            _generatedEdtis = new Dictionary<string, List<string>>();
+            _generatedEdtis = new Dictionary<int, string[]>();
             _generatedFrequencies = new Dictionary<string, long>();
             LoadFrequencies();
-            for (int i = 0; i < _generatedFrequencies.Count; i++)
-            {
-                AddWordToDictionary(_generatedFrequencies.ElementAt(i).Key, null);
-            } 
             Console.WriteLine("SymSpell files loaded successfully");
         }
 
@@ -31,64 +27,41 @@ namespace spell_check.SpelingClasses
         {
             if (_generatedFrequencies.ContainsKey(word)) return word;
             var size = word.Length * (word.Length - 1) / 2;
-            var deletes = GenerateEditsForWord(word, null, new string[size], 0);
-            for (int i = 0; i < deletes.Length; i++)
+            var deletes = GenerateEditsForWord(word, null, new HashSet<string>());
+            foreach (var delete in deletes)
             {
-                if (_generatedEdtis.ContainsKey(deletes[i]))
+                var hash = delete.GetHashCode();
+                string[] value = null;
+                if (_generatedEdtis.TryGetValue(hash, out value))
                 {
-                    var values = _generatedEdtis[deletes[i]];
-                    if (values.Count > 1)
+                    if (value.Length == 1)
                     {
-                        //
+                        return value[0];
                     }
                     else
                     {
-                        return _generatedEdtis[deletes[i]].First();
+                        return value[0];
                     }
                 }
             }
             return "Not found";
         }
 
-        private string[] GenerateEditsForWord(string word, string editedWord, string[] arr, int k)
+        private HashSet<string> GenerateEditsForWord(string word, string editedWord, HashSet<string> set)
         {
             var deletes = editedWord != null ? Deletes(editedWord) : Deletes(word);
             for (int i = 0; i < deletes.Length; i++)
             {
                 var distance = DamerauLevenshtein.Distance(word, deletes[i]);
-                if (distance > _editDistance)
+                if (distance > _editDistance || string.IsNullOrWhiteSpace(deletes[i]))
                 {
                     break;
                 }
 
-                arr[k] = deletes[i];
-                k++;
-                GenerateEditsForWord(word, deletes[i], arr, k);
+                set.Add(deletes[i]);
+                GenerateEditsForWord(word, deletes[i], set);
             }
-            return arr;
-        }
-        
-        private void AddWordToDictionary(string word, string editedWord)
-        {
-            var deletes = editedWord != null ? Deletes(editedWord) : Deletes(word);
-            for (int i = 0; i < deletes.Length; i++)
-            {
-                var distance = DamerauLevenshtein.Distance(word, deletes[i]);
-                if (distance > _editDistance)
-                {
-                    break;
-                }
-
-                if (_generatedEdtis.ContainsKey(deletes[i]))
-                {
-                    _generatedEdtis[deletes[i]].Add(word);
-                }
-                else
-                {
-                    _generatedEdtis.Add(deletes[i], new List<string>(){word});
-                }
-                AddWordToDictionary(word, deletes[i]);
-            }
+            return set;
         }
 
         private string[] Deletes(string word)
@@ -103,12 +76,30 @@ namespace spell_check.SpelingClasses
 
         private void LoadFrequencies()
         {
-            var lines = File.ReadAllLines(@"TextFiles\frequency_dictionary.txt");
+            var lines = File.ReadAllLines(@"TextFiles/frequency_dictionary.txt");
             for (long i = 0; i < lines.Length; i++)
             {
                 var wordF = lines[i].Split(" ");
                 if (wordF.Length < 2) continue;
+                if (long.Parse(wordF[1]) < Threshold) continue;
                 _generatedFrequencies.Add(wordF[0], long.Parse(wordF[1]));
+                var deletes = GenerateEditsForWord(wordF[0], null, new HashSet<string>());
+                foreach (var delete in deletes)
+                {
+                    var hash = delete.GetHashCode();
+                    string[] value = null;
+                    if (_generatedEdtis.TryGetValue(hash, out value))
+                    {
+                        var extendedEdits = new string[value.Length + 1];
+                        Array.Copy(value, extendedEdits, value.Length);
+                        extendedEdits[value.Length] = delete;
+                        _generatedEdtis[hash] = extendedEdits;
+                    }
+                    else
+                    {
+                        _generatedEdtis.Add(hash, new string[1] {wordF[0]});
+                    }
+                }
             }
         }
     }
