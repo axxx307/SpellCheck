26a02bb92f3fd8c2548a5cd0ab6022fbe31002a2
axxx307 <mark.pfnov@gmail.com>
2018-04-10 15:29:32 +0700

    Add AutoComplete project; move project to separate folders

---
+++ AutoComplete/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AutoComplete
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
