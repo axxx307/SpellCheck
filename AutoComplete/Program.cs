b9f3fb5042ca2fd1fb565abb8230ec6ae0e393ba
axxx307 <mark.pfnov@gmail.com>
2018-04-10 15:33:31 +0700

    Add gitignore entries and adjust Program.cs

--- AutoComplete/Program.cs
+++ AutoComplete/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using Autofac;
+using spell_check;
+using spell_check.SpelingClasses;
 
 namespace AutoComplete
 {
@@ -6,7 +9,49 @@ namespace AutoComplete
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var symSpell = new SymSpell(2);
+
+            var isRunning = true;
+            while (isRunning)
+            {
+                Console.WriteLine("Enter mode");
+                var mode = Console.ReadLine();
+                switch (mode)
+                {
+                    case @"\test":
+                    {
+                        Console.WriteLine("Starting tests");
+                        var data = CorrectionTest.TestAgainstWords(symSpell.LookUp);
+                        Console.WriteLine($"Right: {data.Item1}; Wrong: {data.Item2}");
+                        continue;
+                    }
+                    case @"\norvig":
+                    {
+                        var autofac = new AutoFac();
+                        var _container = autofac.AutoFacInit();
+                        var spells = _container.Resolve<Spell>();
+                        var words = new Words();
+                        Console.WriteLine("Enter the word");
+                        var word = Console.ReadLine();
+                        var spelling = spells.Candidates(word, words);
+                        Console.WriteLine($"Corrected to: {spelling}");
+                        continue;
+                    }
+                    case @"\symspell":
+                    {
+                        Console.WriteLine("You can start typing");
+                        var word = Console.ReadLine();
+                        var spelling = symSpell.LookUp(word);
+                        Console.WriteLine($"Corrected to: {spelling}");
+                        continue;
+                    }
+                    case @"\exit":
+                    {
+                        isRunning = false;
+                        break;
+                    }
+                }
+            }
         }
     }
 }
