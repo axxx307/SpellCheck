1685f96eea33879ec725a079fa2593886653ae87
axxx307 <mark.pfnov@gmail.com>
2018-05-02 15:52:46 +0700

    Trie Get method POC, will rework

--- SpellCheckLibrary/SpelingClasses/Trie.cs
+++ SpellCheckLibrary/SpelingClasses/Trie.cs
@@ -21,27 +21,28 @@ namespace SpellCheckLibrary.SpelingClasses
 
             public string Value = string.Empty;
 
-            public bool IsWord = false; 
+            public List<TrieNodeWord> Words = new List<TrieNodeWord>();
+        }
+
+        public class TrieNodeWord
+        {
+            public string Word = string.Empty;
 
             public long Frequency = 0;
         }
 
-        public void Add(string word, long frequency)
+        public void Add(string originalWord, string word, long frequency)
         {
             if (_currentNode.Children?.FirstOrDefault(x=>x.Value == word[0].ToString()) == null)
             {
-                var newNode = new TrieNode
-                {
-                    Value = word[0].ToString(),
-                    IsWord = word.Length == 1,
-                    Frequency = word.Length == 1 ? 0 : frequency
-                };
+                var newNode = new TrieNode{ Value = word[0].ToString() };
+                if (word.Length == 1) newNode.Words.Add(new TrieNodeWord{ Word = originalWord, Frequency = frequency });
                 _currentNode.Children.Add(newNode);
             }
             if (word.Length > 1)
             {
                 _currentNode = _currentNode.Children.First(x => x.Value == word[0].ToString());
-                Add(word.Remove(0, 1), frequency);
+                Add(originalWord, word.Remove(0, 1), frequency);
             }
             _currentNode = _node;
         }
@@ -50,16 +51,28 @@ namespace SpellCheckLibrary.SpelingClasses
         {
             TrieNode nodeWord = new TrieNode();
             var modified = word.Clone().ToString();
-            while(!nodeWord.IsWord)
+            while(modified.Length != 0)
             {
                 nodeWord =  _currentNode.Children.First(x=>x.Value == modified[0].ToString());
                 modified = modified.Remove(0, 1);
                 _currentNode = nodeWord;
             }
-            if (nodeWord.Children != null && !nodeWord.Children.Any(x=>x.IsWord))
+            if (nodeWord.Children != null && nodeWord.Children.Any(x=>x.Words.Count > 0))
             {
-                Debug.WriteLine("implement finding deaper");
-                return new HashSet<string>();
+                int depth = 0;
+                HashSet<string> suggestions = new HashSet<string>() {word};
+                while(depth != 2)
+                {
+                    depth++;
+                    var childValues = _currentNode.Children.Where(f=>f.IsWord).Select(x=> x.Value);
+                    foreach (var item in suggestions)
+                    {
+                        childValues.Select(f=> suggestions.Add(item + f));
+                    }
+                    
+                }
+                _currentNode = _node;
+                return suggestions;
             }
             _currentNode = _node;
             return nodeWord.Children.Where(f=>f.IsWord).OrderBy(f=>f.Frequency).Select(z=> word +z.Value).Take(3).ToHashSet();
